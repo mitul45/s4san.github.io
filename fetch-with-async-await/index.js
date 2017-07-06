4a1210a68ec9af1e13eb4881d2de136667923423
s4san <sandeepkt18@yahoo.com>
2017-07-07 00:15:02 +0530

    More examples

--- fetch-with-async-await/index.js
+++ fetch-with-async-await/index.js
@@ -1,14 +1,38 @@
 +async function ipFetcher() {
-  function setUserIp(ip) {
-    window.setTimeout(() => document.getElementById('retrieved-ip').innerHTML = ip, 500);
+  "use strict";
+  let times = 0;
+  function updateHTML(id, data) {
+    console.log("updating", id);
+    const delay = 500;
+    ++times;
+    window.setTimeout(() => {
+      console.log("complete", id);
+      document.getElementById(id).innerHTML = data;
+    }, delay * times);
   }
 
   try {
+    const postData = JSON.stringify({
+      pi: 3.1415926535
+    });
+    const init = {
+      method: "POST",
+      body: postData
+    };
     const askForIp = await fetch("https://httpbin.org/ip");
-    const ip = await askForIp.json();
+    const ipResponse = await askForIp.json();
 
-    setUserIp(ip.origin);
+    updateHTML('retrieved-ip', ipResponse.origin);
+
+    const postPi = await fetch("https://httpbin.org/post", init);
+    const piResponse = await postPi.json();
+
+    updateHTML('retrieved-pi', piResponse.json.pi);
+
+    const getError = await fetch("https://httpbin.org/hidden-basic-auth/admin/admin");
+    const errorResponse = await getError.json();
   } catch (err) {
+    updateHTML('retrieved-err', err);
     console.error(err);
   }
 }();
